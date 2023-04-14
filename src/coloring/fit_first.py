1e4b70834c26ac603e52264ce0b30b6e8b850299
Sahil Pattni <sahilpattni97@gmail.com>
2023-04-14 16:58:23 -0400

    Updated with gitignore

--- src/coloring/fit_first.py
+++ src/coloring/fit_first.py
@@ -16,3 +16,17 @@ def fit_first(G: nx.Graph) -> nx.Graph:
     G : nx.Graph
         Colored graph.
     """
+    nodes = list(G.nodes())
+    for node in nodes:
+        # Find the colors of neighboring nodes
+        neighbor_colors = set(
+            G.nodes[n]['group'] for n in G.neighbors(node) if 'group' in G.nodes[n]
+            )
+        
+        # Choose the smallest unused color
+        for color in range(len(G)):
+            if color not in neighbor_colors:
+                G.nodes[node]['group'] = color
+                break
+    
+    return G
